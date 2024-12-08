78d3a5ef5bd58da937d1767528929247db9d981d
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2024-12-08 22:33:16 +0100

    🐛 Fix EventTasksDataSourceInitializer

--- logsnag-kotlin/src/androidMain/kotlin/com/logsnag/kotlin/init/EventTasksDataSourceInitializer.kt
+++ logsnag-kotlin/src/androidMain/kotlin/com/logsnag/kotlin/init/EventTasksDataSourceInitializer.kt
@@ -3,16 +3,16 @@ package com.logsnag.kotlin.init
 import android.content.Context
 import androidx.startup.Initializer
 import androidx.work.WorkManager
+import androidx.work.WorkManagerInitializer
 import com.logsnag.kotlin.api.SendEventTasksDataSource
 
-@Suppress("unused")
-internal class EventTasksDataSourceInitializer : Initializer<Unit> {
+public class EventTasksDataSourceInitializer : Initializer<Unit> {
     override fun create(context: Context) {
         val workManager = WorkManager.getInstance(context)
         SendEventTasksDataSource.init(workManager)
     }
 
-    override fun dependencies(): MutableList<Class<out Initializer<*>>> {
-        return mutableListOf()
+    override fun dependencies(): List<Class<out Initializer<*>>> {
+        return listOf(WorkManagerInitializer::class.java)
     }
 }
